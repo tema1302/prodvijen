b4a284700fda62effd62e57f9ab11bd2a75e92f2
= <=>
2021-10-21 20:58:36 +0300

    adaptive for header etc.

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,9 @@
-const s1_Blocks = document.querySelectorAll('.s-1__white-block');
-
-function displayImges(el) {
-    el.target.closest('.s-1__wrapper').querySelector('.img-hover').classList.add('displaying');
-}
-
-function hideImges(el) {
-    console.log(el.target)
-    console.log(el.target.closest('.s-1__wrapper').querySelector('.img-hover'))
-
-    el.target.closest('.s-1__wrapper').querySelector('.img-hover').classList.remove('displaying');
-}
-
-for (let i = 0; i < s1_Blocks.length; i++) {
-    s1_Blocks[i].onmouseenter = displayImges;
-    s1_Blocks[i].onmouseleave = hideImges;
-}
+$(document).ready(function(){
+    $('.s-3__slider').slick({
+        infinite: true,
+        arrows: false,
+        slidesToShow: 3,
+        variableWidth: true,
+        slidesToScroll: 1
+    });
+});  
