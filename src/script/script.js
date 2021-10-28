989d07fcabda428bc91e34e5deeb73003542c30e
= <=>
2021-10-28 15:47:28 +0300

    I hope this is final stylization for desktop

--- src/script/script.js
+++ src/script/script.js
@@ -6,4 +6,9 @@ $(document).ready(function(){
         variableWidth: true,
         touchThreshold: 20,
     });
+    $('.s-3__slider').on('click', '.slick-slide', function () {
+        var slideno = $(this).data('slick-index');
+        console.log(slideno);
+        $('.s-3__slider').slick('slickGoTo', slideno);
+    });
 });  
